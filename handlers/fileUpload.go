81b54e1a2b24a7abf207007010a0945533df0bdf
Seemie <chenios@foxmail.com>
2023-10-24 16:06:41 +0800

    bug fix

--- handlers/fileUpload.go
+++ handlers/fileUpload.go
@@ -12,8 +12,11 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"unsafe"
 	"wios_server/conf"
 )
+
+var cmdMap = make(map[int64]*UploadCommand)
 
 func HandleFileUpload(db *sql.DB) func(ctx *gin.Context) {
 	return func(c *gin.Context) {
@@ -29,8 +32,8 @@ func HandleFileUpload(db *sql.DB) func(ctx *gin.Context) {
 			log.Println("Failed to upgrade connection to WebSocket:", err)
 			return
 		}
-		defer conn.Close()
-
+		address := int64(uintptr(unsafe.Pointer(conn)))
+		defer release(conn, address)
 		for {
 			// 读取消息
 			msgType, msg, err := conn.ReadMessage()
@@ -39,21 +42,27 @@ func HandleFileUpload(db *sql.DB) func(ctx *gin.Context) {
 				break
 			}
 			// 处理消息
-			log.Println("Received message:", string(msg))
 			if msgType == websocket.TextMessage {
+				log.Println("Received string message:", string(msg))
 				var fileInfo = FileInfo{}
 				err := json.Unmarshal(msg, &fileInfo)
 				if err != nil {
 					fmt.Println("Error:", err)
 					return
 				}
-				go processStrMsg(&fileInfo, conn)
+				processStrMsg(&fileInfo, conn, address)
 			}
 			if msgType == websocket.BinaryMessage {
-				go processBinMsg(msg, conn)
+				log.Println("Received binary message:", len(msg))
+				processBinMsg(msg, conn, address)
 			}
 		}
 	}
+}
+func release(cnn *websocket.Conn, cnnAddress int64) {
+	cnn.Close()
+	//cancel command
+	delete(cmdMap, cnnAddress)
 }
 func pushStrMsg(msg string, cnn *websocket.Conn) error {
 	return cnn.WriteMessage(websocket.TextMessage, []byte(msg))
@@ -61,20 +70,22 @@ func pushStrMsg(msg string, cnn *websocket.Conn) error {
 func pushBinMsg(msg []byte, cnn *websocket.Conn) error {
 	return cnn.WriteMessage(websocket.BinaryMessage, msg)
 }
-func processStrMsg(fileInfo *FileInfo, cnn *websocket.Conn) {
-	isExist, data := CheckExist(fileInfo)
-	if isExist {
-		pushBinMsg(data, cnn)
-		return
-	}
-	pushBinMsg(data, cnn)
+func processStrMsg(fileInfo *FileInfo, cnn *websocket.Conn, cnnAddress int64) {
+	CheckExist(fileInfo, cnn, cnnAddress)
 }
-func processBinMsg(msg []byte, cnn *websocket.Conn) {
-	bid := msg[:8]
+func processBinMsg(msg []byte, cnn *websocket.Conn, cnnAddress int64) {
+	bid := msg[:32]
 	id := hex.EncodeToString(bid)
 	fa := agentMap[id]
 
-	cmd := &UploadCommand{Id: bid}
+	cmd := cmdMap[cnnAddress]
+
+	if cmd == nil {
+		cmd = &UploadCommand{Id: bid}
+		cmdMap[cnnAddress] = cmd
+	} else {
+		fa.CommandComplete(cmd, msg[32:])
+	}
 	fa.NextCommand(cmd)
 	pushBinMsg(cmd.toBytes(), cnn)
 }
@@ -91,7 +102,7 @@ type FileAgent struct {
 	IStart    int64
 	AgentFile *os.File
 	Complete  bool
-	UpCount   int64
+	Uploaded  int64
 }
 
 func (fa *FileAgent) NextCommand(cmd *UploadCommand) *FileAgent {
@@ -102,13 +113,18 @@ func (fa *FileAgent) NextCommand(cmd *UploadCommand) *FileAgent {
 	if cmd.End > fa.Size {
 		cmd.End = fa.Size
 	}
-	cmd.Uploaded = fa.UpCount
+	cmd.Uploaded = fa.Uploaded
 	cmd.Total = fa.Size
+	cmd.Idx = i
 	return fa
 }
 func (fa *FileAgent) CommandComplete(cmd *UploadCommand, data []byte) {
-	fa.Complete = true
-
+	fa.Uploaded += cmd.End - cmd.Start
+	SetPositionValue(fa.BSet, 64+cmd.Idx)
+	if fa.Uploaded == fa.Size {
+		log.Println(fa.BSet)
+		fa.Complete = true
+	}
 }
 
 type UploadCommand struct {
@@ -117,6 +133,7 @@ type UploadCommand struct {
 	End      int64
 	Uploaded int64
 	Total    int64
+	Idx      int64
 }
 
 func (cmd *UploadCommand) toBytes() []byte {
@@ -124,7 +141,8 @@ func (cmd *UploadCommand) toBytes() []byte {
 		Int64ToBytes(cmd.Start,
 			cmd.End,
 			cmd.Uploaded,
-			cmd.Total)...)
+			cmd.Total,
+			cmd.Idx)...)
 }
 func Int64ToBytes(values ...int64) []byte {
 	var result []byte
@@ -140,14 +158,14 @@ const BlobSize = 524288
 
 var agentMap = make(map[string]*FileAgent)
 
-func CheckExist(fileInfo *FileInfo) (bool, []byte) {
+func CheckExist(fileInfo *FileInfo, cnn *websocket.Conn, cnnAddress int64) {
 	fid := fileInfo.Id
 	bid, _ := hex.DecodeString(fid)
 	a := uint(bid[0]) >> 6
 	b := uint(bid[1]) >> 6
 	basePath := filepath.Join(conf.Cfg.FileServer.SaveDir, fmt.Sprintf("%d/%d/%s", a, b, fid))
 	if _, err := os.Stat(filepath.Join(basePath, "0")); err == nil {
-		return true, bid
+		pushBinMsg(bid, cnn)
 	} else {
 		fa := agentMap[fid]
 		if fa == nil {
@@ -156,9 +174,11 @@ func CheckExist(fileInfo *FileInfo) (bool, []byte) {
 				panic(err)
 			}
 		}
-		cmd := UploadCommand{Id: bid}
-		fa.NextCommand(&cmd)
-		return false, cmd.toBytes()
+		agentMap[fid] = fa
+		cmd := &UploadCommand{Id: bid}
+		fa.NextCommand(cmd)
+		cmdMap[cnnAddress] = cmd
+		pushBinMsg(cmd.toBytes(), cnn)
 	}
 }
 func NewFileAgen(fileInfo *FileInfo, basePath string) (*FileAgent, error) {
@@ -232,7 +252,7 @@ func GetIStart(bSet []byte, iStart int64) int64 {
 	return -1
 }
 func SetPositionValue(bSet []byte, i int64) {
-	bSet[i>>3] |= 1 << (8 - (i & 7))
+	bSet[i>>3] |= 1 << (7 - (i & 7))
 }
 
 func NewBSet(fSize int64) []byte {
