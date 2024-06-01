59c1af0af5773d92e7fb441fe8bc659f16713613
Seemie <chenios@foxmail.com>
2024-06-01 15:12:11 +0800

    bug fix

--- service/storeUserService.go
+++ service/storeUserService.go
@@ -60,8 +60,8 @@ func (dao *StoreUserService) CheckExist(e *entity.StoreUser) bool {
 func (dao *StoreUserService) Owner(userId *string, wppId *string) bool {
 	sm := mysql.SELECT("COUNT(1)").
 		FROM(entity.PurchasedWpp{}, "p").
-		WHERE("w.user_id = ?", userId).
-		AND("w.wpp_id = ?", wppId).
+		WHERE("p.user_id = ?", userId).
+		AND("p.wpp_id = ?", wppId).
 		LIMIT(1)
 	return dao.ExecuteSM(sm).Count() > 0
 }
